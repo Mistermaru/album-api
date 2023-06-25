13d0de7557236245f794ce1626e86539de8e5487
Mistermaru <mistermaru_@live.nl>
2023-06-25 23:38:59 +0200

    rthrtherger

--- Album-Api/Album.Api/Startup.cs
+++ Album-Api/Album.Api/Startup.cs
@@ -35,9 +35,8 @@ namespace Album.Api
             services.AddRazorPages();
             services.AddSwaggerGen(c => {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "Album API", Version = "v1" });
-                var baseDirectory = AppDomain.CurrentDomain.BaseDirectory;
                 var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.XML";
-                var xmlPath = Path.Combine(baseDirectory, xmlFile);
+                var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
 
                 //... and tell Swagger to use those XML comments.
                 c.IncludeXmlComments(xmlPath, includeControllerXmlComments: true);
